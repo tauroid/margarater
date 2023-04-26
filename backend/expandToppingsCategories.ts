1c8b1b23dbd4ab896910bfd081b42672795406fb
tauroid <adamjacobs173@googlemail.com>
2023-04-26 10:31:42 +0100

    comments resolved hopefully

--- backend/expandToppingsCategories.ts
+++ backend/expandToppingsCategories.ts
@@ -7,14 +7,24 @@ export default async function expandToppingsCategories(
 {
   const categoriesCollection = await getCategoriesCollection()
   const categoriesCache: Record<number, string> = {}
-  return Promise.all(toppings.map(async topping => {
-    if (!(topping.category in categoriesCache)) {
-      const category = await categoriesCollection.findOne({id: topping.category})
+
+  async function getCategoryName(categoryId: number) {
+     if (!(categoryId in categoriesCache)) {
+      const category = await categoriesCollection.findOne({id: categoryId})
       if (category === null) {
         throw new Error('Topping category doesn\'t exist')
       }
-      categoriesCache[topping.category] = category.name
+      categoriesCache[categoryId] = category.name
     }
-    return {...topping, category: categoriesCache[topping.category]}
-  }))
+    return categoriesCache[categoryId]
+  }
+
+  return Promise.all(
+    toppings.map(
+      async topping => ({
+        ...topping,
+        category: await getCategoryName(topping.category)
+      })
+    )
+  )
 }
--- backend/routes/twoRandomToppings.ts
+++ backend/routes/twoRandomToppings.ts
@@ -1,27 +1,33 @@
 import { Request, Response } from 'express'
 import expandToppingsCategories from '../expandToppingsCategories'
 import { getToppingsCollection } from '../db'
-import { ToppingWithCategoryId } from '../topping'
+import { SuccessResponse } from '../response'
+import { Topping, ToppingWithCategoryId } from '../topping'
+
+type TwoRandomToppingsResponse = SuccessResponse<[Topping,Topping]>
 
 export default async function twoRandomToppings(
-  request: Request, response: Response
+  request: Request, response: Response<TwoRandomToppingsResponse>
 ) {
   const exclude = request.query.exclude as (string | null)
 
   const toppingsCollection = await getToppingsCollection()
-  let toppings = null
+  let toppings: null | [ToppingWithCategoryId,ToppingWithCategoryId] = null
 
-  while (
-    !toppings ||
-      (exclude && toppings.some(topping =>
-        exclude.split(',').some((id: string) =>
-          id === topping._id.toString())))
-  ) {
+  const forbiddenToppings =
+    (toppings: ToppingWithCategoryId[]) => (
+      exclude &&
+        toppings.some(topping =>
+          exclude === topping._id.toString())
+    )
+
+  while (!toppings || forbiddenToppings(toppings)) {
     toppings = await toppingsCollection
       .aggregate([{$sample: {size: 2}}])
-      .toArray() as ToppingWithCategoryId[]
+      .toArray() as [ToppingWithCategoryId, ToppingWithCategoryId]
   }
 
   response.json({message: "Successfully retrieved toppings",
-                 data: await expandToppingsCategories(toppings)})
+                 data: (await expandToppingsCategories(toppings)
+                       ) as [Topping, Topping]})
 }
