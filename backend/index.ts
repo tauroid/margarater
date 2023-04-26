2edbcbf0959155289764fafc1c6d528cf26e338d
iO Academy <hamyas@gmail.com>
2023-04-26 12:25:12 +0100

    backend headers for cors

--- backend/index.ts
+++ backend/index.ts
@@ -5,6 +5,13 @@ const app = express()
 const port = 4000
 
 
+app.use((req,res, next)=>{
+    res.setHeader('Access-Control-Allow-Origin',"http://localhost:3000");
+    res.setHeader('Access-Control-Allow-Headers',"*");
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next();
+});
+
 app.get('/', function (request, response) {
   response.send('helloooo)')
 })
