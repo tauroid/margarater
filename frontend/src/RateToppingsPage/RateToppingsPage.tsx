2b8d8dba25fa5f1d05148aebd1ef60b4689bcd91
iO Academy <hamyas@gmail.com>
2023-04-26 16:13:15 +0100

    started styling page

--- frontend/src/RateToppingsPage/RateToppingsPage.tsx
+++ frontend/src/RateToppingsPage/RateToppingsPage.tsx
@@ -1,19 +1,19 @@
 // imports
 import { useState, useEffect, ReactElement } from "react";
 import { JsxElement } from "typescript";
-
+import './RateToppingsPage.scss'
 import RateToppingCard from "../RateToppingCard/RateToppingCard";
 import { Topping } from "../RateToppingCard/RateToppingCard"
 
 const RateToppingsPage = (): JSX.Element => {
-    
-    const dummyTopping: Topping  = {
+
+    const dummyTopping: Topping = {
         _id: '',
         name: '',
-        category: '', 
-        img: '', 
+        category: '',
+        img: '',
     }
-    
+
     const [topping1, setTopping1] = useState<Topping>(dummyTopping)
     const [topping2, setTopping2] = useState<Topping>(dummyTopping)
 
@@ -35,13 +35,17 @@ const RateToppingsPage = (): JSX.Element => {
 
     return (
 
-        <>
+        <div className="page">
+
             <h2>Tell us your tastiest topping out of these two!</h2>
 
-            <RateToppingCard cardTopping={topping1} get2RandomToppings={get2RandomToppings} />
-
-            <RateToppingCard cardTopping={topping2} get2RandomToppings={get2RandomToppings}/>
-        </>
+            <div className="cardsDisplay">
+                <RateToppingCard cardTopping={topping1} get2RandomToppings={get2RandomToppings} />
+                <span>VS</span>
+                <RateToppingCard cardTopping={topping2} get2RandomToppings={get2RandomToppings} />
+            </div>
+            
+        </div>
 
 
     )
